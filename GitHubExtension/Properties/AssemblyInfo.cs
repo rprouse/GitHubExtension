9aac8175cf9a942c2c1cc56594d18769b67590aa
Rob Prouse <rob@prouse.org>
2014-02-24 14:58:22 -0500

    Switched the extension to version 0.1, added a license and changelog.

--- GitHubExtension/Properties/AssemblyInfo.cs
+++ GitHubExtension/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: InternalsVisibleTo("GitHubExtension_IntegrationTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010027eecf879a076368146999cfd15ec66f5bfe253f864e38133f88c8b95f39b9ee7f033de2acdf44b5c2c8555d98fc5ff1f76028dd22032e6346bb3ffc443495fd04eacfdb68d56d37401e3d350886386a6ebf697db1a1a7268b86cca120d1450f4b28c771abb1363ecf7d83ac0dc0e388641ca11a971a90b4cd414fb142d8f2c8")]
 [assembly: InternalsVisibleTo("GitHubExtension_UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010027eecf879a076368146999cfd15ec66f5bfe253f864e38133f88c8b95f39b9ee7f033de2acdf44b5c2c8555d98fc5ff1f76028dd22032e6346bb3ffc443495fd04eacfdb68d56d37401e3d350886386a6ebf697db1a1a7268b86cca120d1450f4b28c771abb1363ecf7d83ac0dc0e388641ca11a971a90b4cd414fb142d8f2c8")]
