1212793b0be662d43f017acea1aae56e5e0c2642
Rob Prouse <rob@prouse.org>
2014-03-05 22:18:53 -0500

    Fix #9 using the MarkdownDeep control, this doesn't fully support the GitHub extensions, so I am going to try MarkdownSharp too, but want to checkpoint my work. This will be good enough otherwise.

---
+++ GitHubExtension/View/WebBrowserBehavior.cs
@@ -0,0 +1,57 @@
+﻿// **********************************************************************************
+// The MIT License (MIT)
+// 
+// Copyright (c) 2014 Rob Prouse
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+// **********************************************************************************
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Alteridem.GitHub.Extension.View
+{
+    public class WebBrowserBehavior
+    {
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached(
+                "Html",
+                typeof(string),
+                typeof(WebBrowserBehavior),
+                new FrameworkPropertyMetadata(OnHtmlChanged));
+
+        [AttachedPropertyBrowsableForType(typeof(WebBrowser))]
+        public static string GetHtml(WebBrowser d)
+        {
+            return (string)d.GetValue(HtmlProperty);
+        }
+
+        public static void SetHtml(WebBrowser d, string value)
+        {
+            d.SetValue(HtmlProperty, value);
+        }
+
+        static void OnHtmlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            WebBrowser webBrowser = dependencyObject as WebBrowser;
+            if (webBrowser != null)
+                webBrowser.NavigateToString(e.NewValue as string);
+        }
+         
+    }
+}
