e7e12eead0f91d943bcda993c6ae2584439d1d9a
Sam Harwell <sam.harwell@rackspace.com>
2014-12-03 13:40:17 -0600

    Force a build error if the Secrets.cs file is not updated

--- GitHubIssues/Model/Secrets.cs
+++ GitHubIssues/Model/Secrets.cs
@@ -2,6 +2,7 @@
 {
     internal static class Secrets
     {
+#error Update the CLIENT_ID and CLIENT_SECRET on the following lines and remove this error, as described in the README file.
         internal const string CLIENT_ID = "FILL_IN";
         internal const string CLIENT_SECRET = "FILL_IN"; 
     }
