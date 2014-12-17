1d0bb9c9f4c4e053f3bee6e5b95d139b16dcd79d
Rob Prouse <rob@prouse.org>
2014-12-16 21:04:25 -0500

    Added an Enum converter

--- GitHubIssues/Model/GitHubApiBase.cs
+++ GitHubIssues/Model/GitHubApiBase.cs
@@ -25,7 +25,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
