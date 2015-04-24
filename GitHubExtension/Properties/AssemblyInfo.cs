32223d6e59021b04fabf0379acac9ac2e28a8252
Rob Prouse <rob@prouse.org>
2015-04-24 15:15:21 -0400

    Updated the version to 0.5

--- GitHubExtension/Properties/AssemblyInfo.cs
+++ GitHubExtension/Properties/AssemblyInfo.cs
@@ -7,30 +7,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GitHub Extension")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Alteridem Consulting")]
-[assembly: AssemblyProduct("GitHub Extension")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]   
-[assembly: ComVisible(false)]     
-[assembly: CLSCompliant(false)]
-[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyTitle("GitHubExtension")]
+[assembly: AssemblyDescription("A Visual Studio extension for working with GitHub")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
-
-[assembly: InternalsVisibleTo("GitHubExtension_IntegrationTests")]
-[assembly: InternalsVisibleTo("GitHubExtension_UnitTests")]
+[assembly: InternalsVisibleTo("GitHubExtension.Test")]
---
+++ GitHubIssues/AssemblyInfoCommon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alteridem Consulting")]
+[assembly: AssemblyProduct("GitHub Visual Studio Extension")]
+[assembly: AssemblyCopyright("Copyright © 2015, Rob Prouse")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
